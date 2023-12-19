be1c66a9d7b68395ed65e1c6c6138322f15dd0bf
Jake <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2023-12-19 09:21:58 -0600

    Updated to mitosis 0.3

--- run_exps.sh
+++ run_exps.sh
@@ -1,99 +1,117 @@
     # --param other_params=lor-ross-cubic \
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=lorenz \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g lorenz \
+    -F trials \
     --param metrics=all \
     --param other_params=lor-ross-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> lorenz.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> lorenz.log &
 
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=cubic_ho \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g cubic_ho \
+    -F trials \
     --param metrics=all \
     --param other_params=4nonzero-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> cubic_ho.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> cubic_ho.log &
 
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=sho \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g sho \
+    -F trials \
     --param metrics=all \
     --param other_params=4nonzero-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> sho.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> sho.log &
 
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=vdp \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g vdp \
+    -F trials \
     --param metrics=all \
     --param other_params=4nonzero-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> vdp.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> vdp.log &
 
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=lv \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g lv \
+    -F trials \
     --param metrics=all \
     --param other_params=4nonzero-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> lv.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> lv.log &
 
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=duff \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g duff \
+    -F trials \
     --param metrics=all \
     --param other_params=4nonzero-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> duff.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> duff.log &
 
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=hopf \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g hopf \
+    -F trials \
     --param metrics=all \
     --param other_params=hopf-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> hopf.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> hopf.log &
 
-nohup python -m gen_experiments gridsearch --seed 19 \
-    --param ex_name=ross \
+nohup mitosis gen_experiments.gridsearch \
+    -e seed=19 \
+    -g ross \
+    -F trials \
     --param metrics=all \
     --param other_params=lor-ross-cubic \
     --param grid_params=duration-absnoise \
     --param grid_vals=duration-absnoise \
     --param grid_decisions=plot2 \
     --param series_params=kalman-auto3 \
-    --param plot_prefs=test-absrel5 \
-    --param skinny_specs=duration-noise &> ross.log &
+    --param +plot_prefs=test-absrel5 \
+    --param +skinny_specs=duration-noise &> ross.log &
 
-# nohup python -m gen_experiments gridsearch --seed 19 --debug \
-#     --param ex_name=ross \
+# nohup mitosis gen_experiments.gridsearch \
+#     -e seed=19 \
+#     -g ross \
+#     -F trials \
 #     --param metrics=all \
 #     --param other_params=debug \
 #     --param grid_params=duration-absnoise \
 #     --param grid_vals=debug \
 #     --param grid_decisions=plot2 \
-#     --param series_params=kalman-auto3 \
-#     --param plot_prefs=test-absrel5 \
+#     --param +series_params=kalman-auto3 \
+#     --param +plot_prefs=test-absrel5 \
 #     &> ross.log &
